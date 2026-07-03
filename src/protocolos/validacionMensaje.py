d5b6e78dd9588da680e0a6f431cbb92f78d38fed
square0108 <square@heckin.how>
2026-07-02 23:10:50 -0400

    feat(validacion): Verificación de integridad y autenticidad de mensajes mediante hmac y clave simétrica

---
+++ src/protocolos/validacionMensaje.py
@@ -0,0 +1,55 @@
+import hmac, hashlib, json, sys
+
+""" 
+Utilizado por EMISOR. 
+Genera un diccionario Python que contiene al string JSON proporcionado y una firma hash de su contenido.
+
+@param data: Mensaje JSON creado con json.dumps
+@param secret_key: String de llave secreta no hasheada
+@return: Diccionario Python con dos pares clave:valor,
+ 'data', cuyo valor es el payload/mensaje enviado (sin aplicar encriptación)
+ 'digest', cuyo valor es un Message Authentication Code (MAC) utilizando cierta función hash (SHA256 por ahora).
+"""
+
+def firmarMensajeJSON(data: json, secret_key: str) -> dict[str | str]:
+  if not (verificarFormatoJSON(data)):
+    return
+  
+  # hmac.new() requiere encoding UTF-8
+  data_bytes = data.encode('utf-8')
+  secret_key_bytes = secret_key.encode('utf-8')
+
+  mac: hmac = hmac.new(key=secret_key_bytes, 
+                        msg=data_bytes, 
+                        digestmod=hashlib.sha256)
+  
+  output = {"data" : data, "digest" : mac.hexdigest()}
+  return output
+
+"""
+Utilizado por RECEPTOR,
+para validar la autenticidad e integridad del mensaje.
+@param data: Mensaje JSON no encriptado recibido
+@param hexdigest: String digest recibida
+@param secret_key: Clave secreta compartida por cliente y servidor
+@return: 
+  - True si: 
+    1) se comprueba que se usó la misma clave secreta
+    2) se comprueba la integridad del mensaje
+  - False en otro caso
+"""
+def validarMensaje(data: str, hexdigest: str, secret_key: str) -> bool:
+  expectedMAC = hmac.new(key=secret_key.encode('utf-8'),
+                              msg=data.encode('utf-8'),
+                              digestmod=hashlib.sha256)
+  
+  return hmac.compare_digest(expectedMAC.hexdigest(), hexdigest)
+
+""" Verificar que `msg` está en formato JSON """
+def verificarFormatoJSON(msg) -> bool:
+  try:
+    json.loads(msg)
+    return True
+  except (ValueError, TypeError, json.JSONDecodeError):
+    print(f"Error de firma: Mensaje tiene formato inválido. Mensaje: {msg}", file=sys.stderr)
+    return False
