78fc796dac1ee36551e3643a0f891ffcbf3d4e94
square0108 <martinmariollanos@gmail.com>
2026-07-06 23:49:03 -0400

    Fix estilístico de documentación

--- src/protocolos/validacionMensaje.py
+++ src/protocolos/validacionMensaje.py
@@ -1,17 +1,15 @@
 import hmac, hashlib, json, sys
 
-""" 
-Utilizado por EMISOR. 
-Genera un diccionario Python que contiene al string JSON proporcionado y una firma hash de su contenido.
-
-@param data: Mensaje JSON creado con json.dumps
-@param secret_key: String de llave secreta no hasheada
-@return: Diccionario Python con dos pares clave:valor,
- 'data', cuyo valor es el payload/mensaje enviado (sin aplicar encriptación)
- 'digest', cuyo valor es un Message Authentication Code (MAC) utilizando cierta función hash (SHA256 por ahora).
-"""
-
 def firmarMensajeJSON(data: json, secret_key: str) -> dict[str | str]:
+  """ 
+  Utilizado por EMISOR. Genera un diccionario Python que contiene al string JSON proporcionado y una firma hash de su contenido.
+
+  @param data: Mensaje JSON creado con json.dumps
+  @param secret_key: String de llave secreta no hasheada
+  @return: Diccionario Python con dos pares clave:valor,
+  'data', cuyo valor es el payload/mensaje enviado (sin aplicar encriptación)
+  'digest', cuyo valor es un Message Authentication Code (MAC) utilizando cierta función hash (SHA256 por ahora).
+  """
   if not (verificarFormatoJSON(data)):
     return
   
@@ -26,19 +24,16 @@ def firmarMensajeJSON(data: json, secret_key: str) -> dict[str | str]:
   output = {"data" : data, "digest" : mac.hexdigest()}
   return output
 
-"""
-Utilizado por RECEPTOR,
-para validar la autenticidad e integridad del mensaje.
-@param data: Mensaje JSON no encriptado recibido
-@param hexdigest: String digest recibida
-@param secret_key: Clave secreta compartida por cliente y servidor
-@return: 
-  - True si: 
-    1) se comprueba que se usó la misma clave secreta
-    2) se comprueba la integridad del mensaje
-  - False en otro caso
-"""
 def validarMensaje(data: str, hexdigest: str, secret_key: str) -> bool:
+  """
+  Utilizado por RECEPTOR, para validar la autenticidad e integridad del mensaje.
+
+  @param data: Mensaje JSON no encriptado recibido
+  @param hexdigest: String digest recibida
+  @param secret_key: Clave secreta compartida por cliente y servidor
+  @return: - True si la MAC calculada por el receptor coincide con la recibida.
+  - False en otro caso
+  """
   expectedMAC = hmac.new(key=secret_key.encode('utf-8'),
                               msg=data.encode('utf-8'),
                               digestmod=hashlib.sha256)
